7262efc4a661c4efdf7127c9ee08ff8f647977ea
kgs90 <kgs90@inbox.ru>
2022-04-12 14:16:51 +0300

    second2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("введите 1 число :");
+int number1 =
+Convert.ToInt32(Console.ReadLine());
+Console.Write("введите 2 число :");
+int number2 =
+Convert.ToInt32(Console.ReadLine());
+Console.Write("введите 3 число :");
+int number3 =
+Convert.ToInt32(Console.ReadLine());
+if (number1 > number3);
+{
+    System.Console.WriteLine($"number max {+number1}");
+}       
+else if(number2 > number3 && number1 > number2 && number1 > number3);
+{
+    System.Console.WriteLine($"number max{+number2}");
+}
+else;
+{
+    System.Console.WriteLine($"number max{+number3}");
+}
+
+
+
+
+        
+
+
